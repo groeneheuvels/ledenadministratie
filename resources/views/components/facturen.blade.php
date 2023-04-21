1ed44f43e71b6911695f02fb41f7bc36d2aef58b
Tosca van Schaik <toscavanschaik@gmail.com>
2023-04-21 11:35:39 +0200

    feat: stijlen gewijzigd

--- resources/views/components/facturen.blade.php
+++ resources/views/components/facturen.blade.php
@@ -3,23 +3,24 @@
 @endphp
 
 @unless ($openstaandeFacturen->count() == 0)
-    <p>Openstaande facturen:</p>
-    @foreach ($openstaandeFacturen as $factuur)
-        <a href="{{ url('/facturen#' . $factuur->id) }}">
-            <dl>
-                <dt>Factuur, boekjaar:</dt>
-                <dd>{{ $factuur->boekjaar->jaartal }}</dd>
-                <dt>Bedrag:</dt>
-                <dd> € {{ $factuur->factuurbedrag }}</dd>
-            </dl>
-        </a>
-        <form method="POST" action="/facturen/{{ $factuur->id }}">
-            @csrf
-            @method('PUT')
-
-            <div class="form-field"><button type="submit">Markeer als betaald</button></div>
-        </form>
-    @endforeach
+    <div class="kaart">
+        <h4>Openstaande facturen</h4>
+        @foreach ($openstaandeFacturen as $factuur)
+            <a href="{{ url('/facturen#' . $factuur->id) }}">
+                <dl>
+                    <dt>Factuur, boekjaar:</dt>
+                    <dd>{{ $factuur->boekjaar->jaartal }}</dd>
+                    <dt>Bedrag:</dt>
+                    <dd> € {{ $factuur->factuurbedrag }}</dd>
+                </dl>
+            </a>
+            <form method="POST" action="/facturen/{{ $factuur->id }}">
+                @csrf
+                @method('PUT')
+                <div class="form-field"><button type="submit">Markeer als betaald</button></div>
+            </form>
+        @endforeach
+    </div>
 @else
     <p>Geen openstaande facturen gevonden</p>
 @endunless
